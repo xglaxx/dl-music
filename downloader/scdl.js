c0ff3c6908139316d9d5591c4ab4d01e0b314b34
xglaxx <karla68santos@gmail.com>
2024-11-03 18:23:40 -0300

    scdl

--- downloader/scdl.js
+++ downloader/scdl.js
@@ -16,12 +16,10 @@ exports.SoundCloud = class SoundCloud extends Client {
 		
 		url = url[0]
 		switch (type) {
-			case 'track':
-				return url.match(/^https?:\/\/(on\.|)(soundcloud\.com|snd\.sc)\/([A-Za-z0-9_-]+)\/([A-Za-z0-9_-]+)\/?$/)
-			case 'playlist':
-				return url.match(/^https?:\/\/(on\.|)(soundcloud\.com|snd\.sc)\/([A-Za-z0-9_-]+)\/sets\/([A-Za-z0-9_-]+)\/?$/)
+			case 'track': case 'playlist':
+				return url.match(/^https?:\/\/(on\.|)(soundcloud\.com|snd\.sc)\/([A-Za-z0-9_-]+)(?:\/sets\/|\/)([A-Za-z0-9_-]+)/)
 			case "artist":
-				return url.match(/^https?:\/\/(on\.|)(soundcloud\.com|snd\.sc)\/([A-Za-z0-9_-]+)\/?$/)
+				return url.match(/^https?:\/\/(on\.|)(soundcloud\.com|snd\.sc)\/([A-Za-z0-9_-]+)/)
 			case 'user':
 				return url.match(/@([a-zA-Z0-9_-]+)/)
 			default:
